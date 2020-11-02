4e0743c67fe79e8ef83342ad297c6ed76c8856a0
NoxNoscope <Lennart.Diego.kahn@gmail.com>
2020-11-02 08:51:40 +0100

    YWS
    IT FINNALY WORKED
    I GOT IT BUILD

--- build-randomNum-Desktop_Qt_5_15_0_clang_64bit-Debug/moc_mainwindow.cpp
+++ build-randomNum-Desktop_Qt_5_15_0_clang_64bit-Debug/moc_mainwindow.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../../../../Documents/GitHub/random_numberGenerator/randomNum/mainwindow.h"
+#include "../randomNum/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
